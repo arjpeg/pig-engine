2c9a48d733e9684438b9840b046c59cc086aa2c7
arjpeg <araoudupi@gmail.com>
2024-07-04 16:57:09 -0500

    add winit input helper

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use renderer::Renderer;
 use wgpu::SurfaceError;
 use winit::{dpi::LogicalSize, event::Event, event_loop::EventLoop, window::WindowBuilder};
+use winit_input_helper::WinitInputHelper;
 
 mod camera;
 mod model;
@@ -10,40 +11,36 @@ mod renderer;
 
 #[pollster::main]
 async fn main() -> anyhow::Result<()> {
-    use winit::event::WindowEvent as WE;
-
     let event_loop = EventLoop::new()?;
     let window = WindowBuilder::new()
         .with_title("Pig Engine")
         .with_inner_size(LogicalSize::new(1920, 1080))
         .build(&event_loop)?;
 
+    let mut input = WinitInputHelper::new();
+
     let mut renderer = Renderer::new(&window).await?;
 
-    event_loop.run(|event, elwt| match event {
-        Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
-            WE::RedrawRequested => {
-                match renderer.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost
-                    Err(SurfaceError::Lost) => renderer.resize(window.inner_size()),
-                    // If we are out of memory, just quit the app
-                    Err(SurfaceError::OutOfMemory) => elwt.exit(),
-                    // For other errors, they will be gone by the next frame
-                    Err(error) => eprintln!("{error}"),
-                };
-            }
+    event_loop.run(|event, elwt| {
+        if !input.update(&event) {
+            return;
+        };
 
-            WE::Resized(size) => renderer.resize(size),
+        println!("{:?}", input.mouse_diff());
 
-            _ => {}
-        },
-
-        Event::AboutToWait => {
-            window.request_redraw();
+        if let Some(size) = input.window_resized() {
+            renderer.resize(size);
         }
 
-        _ => {}
+        match renderer.render() {
+            Ok(_) => {}
+            // Reconfigure the surface if it's lost
+            Err(SurfaceError::Lost) => renderer.resize(window.inner_size()),
+            // If we are out of memory, just quit the app
+            Err(SurfaceError::OutOfMemory) => elwt.exit(),
+            // For other errors, they will be gone by the next frame
+            Err(error) => eprintln!("{error}"),
+        };
     })?;
 
     Ok(())
